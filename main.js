48e91201ba12d955ca6914ec09646f329510e03d
Kaylee Odom <kaylee.odom@students.fhu.edu>
2024-08-23 21:22:56 -0500

    close enough

--- main.js
+++ main.js
@@ -5,13 +5,36 @@ const hideButton = document.getElementById("hide-share-block-button")
 const personDiv = document.getElementById("profile-info-container")
 const shareDiv = document.getElementById("share-container")
 
+// // Step 2: hook into click events
+// showButton.addEventListener("click", () => {
+//     personDiv.classList.add("hidden")
+//     shareDiv.classList.remove("hidden");
+// })
+
+// hideButton.addEventListener("click", () => {
+//     personDiv.classList.remove("hidden");
+//     shareDiv.classList.add("hidden")
+// })
+
 // Step 2: hook into click events
 showButton.addEventListener("click", () => {
-    personDiv.classList.add("hidden")
-    shareDiv.classList.remove("hidden");
+    if(window.innerWidth > 800) {
+        showButton.classList.toggle("active")
+        shareDiv.classList.toggle("hidden")
+    }
+    else {
+        personDiv.classList.add("hidden")
+        shareDiv.classList.remove("hidden")
+    }
 })
 
 hideButton.addEventListener("click", () => {
-    personDiv.classList.remove("hidden");
-    shareDiv.classList.add("hidden")
+    if(window.innerWidth > 800) {
+        showButton.classList.toggle("active")
+        shareDiv.classList.toggle("hidden")
+    }
+    else {
+        personDiv.classList.remove("hidden")
+        shareDiv.classList.add("hidden")
+    }
 })
